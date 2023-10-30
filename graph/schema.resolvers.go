ff8c3d3b24e6f85e7e452eb3143d3172af2a91ee
taako-502 <i.am.armorknight@gmail.com>
2023-10-30 18:48:14 +0200

    schema.resolverの実装

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -8,16 +8,26 @@ import (
 	"context"
 	"fmt"
 	"go-graphql-nosql/graph/model"
+	"math/rand"
 )
 
 // CreateTodo is the resolver for the createTodo field.
 func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
-	panic(fmt.Errorf("not implemented: CreateTodo - createTodo"))
+	todo := &model.Todo{
+		Text: input.Text,
+		ID:   fmt.Sprintf("T%d", rand.Int()),
+		User: &model.User{
+			ID:   input.UserID,
+			Name: "user" + input.UserID,
+		},
+	}
+	r.todos = append(r.todos, todo)
+	return todo, nil
 }
 
 // Todos is the resolver for the todos field.
 func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	panic(fmt.Errorf("not implemented: Todos - todos"))
+	return r.todos, nil
 }
 
 // Mutation returns MutationResolver implementation.
