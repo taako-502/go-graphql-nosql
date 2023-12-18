9149e7169434ac036b0a5a8397780c69cee2e551
taako-502 <i.am.armorknight@gmail.com>
2023-12-18 11:04:41 +0200

    fix: 最小権限の法則により、CORSの設定を変更

--- handler/internal/config/cors.go
+++ handler/internal/config/cors.go
@@ -13,12 +13,7 @@ func SettingCors(hosts []string) gin.HandlerFunc {
 	return cors.New(cors.Config{
 		AllowOrigins: hosts,
 		AllowMethods: []string{
-			http.MethodGet,
 			http.MethodPost,
-			http.MethodPatch,
-			http.MethodDelete,
-			http.MethodPut,
-			http.MethodOptions,
 		},
 		AllowHeaders:     []string{"*"},
 		AllowCredentials: true,
