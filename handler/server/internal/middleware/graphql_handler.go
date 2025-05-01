5cc3495bd65eb7a4cd85ef94712027e154852eeb
taako-502 <yamamoto.takao502@gmail.com>
2025-05-02 01:53:26 +0900

    fix: ソースコードの最新か

--- handler/server/internal/middleware/graphql_handler.go
+++ handler/server/internal/middleware/graphql_handler.go
@@ -6,16 +6,17 @@ import (
 	"github.com/taako-502/go-graphql-nosql/handler/graph"
 
 	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/guregu/dynamo/v2"
 )
 
 func GraphqlHandler(DB *dynamo.DB, region string) http.HandlerFunc {
 	// FIXME: これは古い書き方なので、新しい書き方Newに変更する
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{
-		Resolvers: &graph.Resolver{
-			DB: DB,
-		},
-	}))
+	c := graph.Config{Resolvers: &graph.Resolver{
+		DB: DB,
+	}}
+	srv := handler.New(graph.NewExecutableSchema(c))
+	srv.AddTransport(transport.POST{})
 	return func(w http.ResponseWriter, r *http.Request) {
 		srv.ServeHTTP(w, r)
 	}
